856a99ae6e01ba0f3a8ad9cb8ff6413d8d138c42
jingtalk <490134273@qq.com>
2023-08-19 14:45:34 +0800

    优化代码逻辑

--- app/src/main/java/com/example/verify/MainActivity.kt
+++ app/src/main/java/com/example/verify/MainActivity.kt
@@ -16,6 +16,9 @@ import com.example.verify.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    // 启用 View Binding 后，系统会为每个 XML 布局文件生成一个绑定类。
+    // 绑定类的名称是将 XML 文件的名称转换为驼峰式大小写，并在末尾添加 "Binding"。
+    // 例如，如果布局文件名为 activity_main.xml，生成的绑定类名为 ActivityMainBinding。
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
